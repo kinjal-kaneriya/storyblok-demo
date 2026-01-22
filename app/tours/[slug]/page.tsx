2ba4a3f72082bf4b76cc243acd8ce2eb50104c2a
kinjal-kaneriya <kinjalk.encircle@gmail.com>
2026-01-22 16:15:00 +0530

    feat: editable storyblok

--- app/tours/[slug]/page.tsx
+++ app/tours/[slug]/page.tsx
@@ -2,26 +2,23 @@ import { getStoryblokApi, StoryblokStory } from "@storyblok/react/rsc";
 import { draftMode } from "next/headers";
 
 export const generateStaticParams = async () => {
-  const {isEnabled} = await draftMode();
-  const client = getStoryblokApi();
-  const response = await client.getStories({
-    content_type: "tour",
-    version: process.env.NODE_ENV === "development" || isEnabled
-      ? "draft"
-      : "published",
-  });
+    const client = getStoryblokApi();
+    const response = await client.getStories({
+        content_type: "tour",
+        version: process.env.NODE_ENV === "development" ? "draft" : "published",
+    });
 
-  return response.data.stories.map((story) => ({ slug: story.slug }));
+    return response.data.stories.map((story) => ({ slug: story.slug }));
 };
 
 const fetchTourPage = async (slug: string) => {
-  const {isEnabled} = await draftMode();
+    const { isEnabled } = await draftMode();
     const client = getStoryblokApi();
     try {
         const response = await client.getStory(`tours/${slug}`, {
             version: process.env.NODE_ENV === "development" || isEnabled
-              ? "draft"
-              : "published",
+                ? "draft"
+                : "published",
         });
         return response.data.story;
     }
